33627baf41fb716316bc6e757ae87bc02bf53ded
Иван Плесских <terma95@gmail.com>
2017-09-25 16:14:43 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
+self.addEventListener('install', function() {
+    self.skipWaiting()
+})
 
+self.addEventListener('fetch', function(e) {
+    e.respondWith(caches.match(e.request).then(function(response) {
+        if (response !== undefined) {
+            return response
+        } else {
+            return fetch(e.request).then(function (resp) {
+                const clone = resp.clone()
+                caches.open('test').then(function (cache) {
+                    cache.put(e.request, clone)
+                })
+                return resp
+            }).catch(function () {
+                return response
+            })
+        }
+    }))
+})
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        self.clients.claim().then(caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                return caches.delete(key)
+            }))
+        }))
+    )
+})
